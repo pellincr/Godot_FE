01ef2576c4e9dae0824e2068c4333d8ee5c00a25
Pellincr <craig.pelling@gmail.com>
2025-08-02 18:26:36 -0400

    Separated army_convoy_sub_container

--- ui/battle_preparation/army_convoy_container.gd
+++ ui/battle_preparation/army_convoy_container.gd
@@ -11,7 +11,7 @@ var playerOverworldData : PlayerOverworldData
 
 @onready var main_scroll_container = $HBoxContainer/ScrollContainer/VBoxContainer
 
-@onready var sub_container = $HBoxContainer
+@onready var army_convoy_sub_container = $ArmyConvoySubContainer
 
 const unit_army_panel_container_scene = preload("res://ui/battle_preparation/unit_army_panel_container.tscn")
 const convoy_item_panel_container_scene = preload("res://ui/battle_preparation/convoy_item_panel_container.tscn")
@@ -25,60 +25,66 @@ enum CONTAINER_STATE{
 var current_container_state = CONTAINER_STATE.ARMY
 
 func _ready():
+	
 	if playerOverworldData == null:
 		playerOverworldData = PlayerOverworldData.new()
-		
+	army_convoy_sub_container.set_po_data(playerOverworldData)
+	
 	var current_selected_count = playerOverworldData.selected_party.size()
 	var max_selected_count = playerOverworldData.available_party_capacity
 	army_convoy_header.set_units_left_value(current_selected_count,max_selected_count)
-	fill_army_scroll_container()
+	army_convoy_sub_container.fill_army_scroll_container()
 
 func set_po_data(po_data):
 	playerOverworldData = po_data
+	army_convoy_sub_container.set_po_data(playerOverworldData)
 
 
 func fill_army_scroll_container():
-	for unit in playerOverworldData.total_party:
-		var unit_army_panel_container = unit_army_panel_container_scene.instantiate()
-		unit_army_panel_container.unit = unit
-		unit_army_panel_container.set_po_data(playerOverworldData)
-		main_scroll_container.add_child(unit_army_panel_container)
-		unit_army_panel_container.focus_entered.connect(unit_focus_entered.bind(unit))
-		unit_army_panel_container.unit_selected.connect(_on_unit_selected)
-		unit_army_panel_container.unit_deselected.connect(_on_unit_deselected)
-	var test = main_scroll_container.get_child(0)
-	if(test):
-		test.grab_focus()
+	army_convoy_sub_container.fill_army_scroll_container()
+#	for unit in playerOverworldData.total_party:
+#		var unit_army_panel_container = unit_army_panel_container_scene.instantiate()
+#		unit_army_panel_container.unit = unit
+#		unit_army_panel_container.set_po_data(playerOverworldData)
+#		main_scroll_container.add_child(unit_army_panel_container)
+#		unit_army_panel_container.focus_entered.connect(unit_focus_entered.bind(unit))
+#		unit_army_panel_container.unit_selected.connect(_on_unit_selected)
+#		unit_army_panel_container.unit_deselected.connect(_on_unit_deselected)
+#	var test = main_scroll_container.get_child(0)
+#	if(test):
+#		test.grab_focus()
 
 func fill_convoy_scroll_container():
-	var i = 0
-	while i < temp:
-		var convoy_item_panel_container = convoy_item_panel_container_scene.instantiate()
-		main_scroll_container.add_child(convoy_item_panel_container)
-		convoy_item_panel_container.focus_entered.connect(item_focus_entered.bind(convoy_item_panel_container.item))
-		if i == 0:
-			convoy_item_panel_container.grab_focus()
-		i += 1
+	army_convoy_sub_container.fill_convoy_scroll_container()
+#	var i = 0
+#	while i < temp:
+#		var convoy_item_panel_container = convoy_item_panel_container_scene.instantiate()
+#		main_scroll_container.add_child(convoy_item_panel_container)
+#		convoy_item_panel_container.focus_entered.connect(item_focus_entered.bind(convoy_item_panel_container.item))
+#		if i == 0:
+#			convoy_item_panel_container.grab_focus()
+#		i += 1
 
 func clear_scroll_scontainer():
-	var children = main_scroll_container.get_children()
-	for child in children:
-		child.queue_free()
+#	var children = main_scroll_container.get_children()
+#	for child in children:
+#		child.queue_free()
+	army_convoy_sub_container.clear_scroll_container()
 
-func get_first_scroll_panel():
-	return main_scroll_container.get_child(0)
+#func get_first_scroll_panel():
+#	return main_scroll_container.get_child(0)
 
 func _on_army_convoy_header_header_swapped():
 	match current_container_state:
 		CONTAINER_STATE.ARMY:
 			current_container_state = CONTAINER_STATE.CONVOY
-			clear_scroll_scontainer()
-			fill_convoy_scroll_container()
+			army_convoy_sub_container.clear_scroll_scontainer()
+			army_convoy_sub_container.fill_convoy_scroll_container()
 		CONTAINER_STATE.CONVOY:
 			current_container_state = CONTAINER_STATE.ARMY
-			clear_scroll_scontainer()
-			fill_army_scroll_container()
-	get_first_scroll_panel().grab_focus()
+			army_convoy_sub_container.clear_scroll_scontainer()
+			army_convoy_sub_container.fill_army_scroll_container()
+	army_convoy_sub_container.get_first_scroll_panel().grab_focus()
 
 
 func unit_focus_entered(unit):
@@ -98,4 +104,4 @@ func _on_unit_deselected(unit):
 	army_convoy_header.set_units_left_value(current_selected_count,max_selected_count)
 
 func get_sub_container():
-	return sub_container
+	return army_convoy_sub_container
---
+++ ui/battle_preparation/army_convoy_sub_container.gd
@@ -0,0 +1,72 @@
+extends HBoxContainer
+
+signal unit_focused()
+signal item_focused()
+signal unit_selected()
+signal unit_deselected()
+
+@onready var main_scroll_container = $ScrollContainer/MainScrollContainer
+
+const unit_army_panel_container_scene = preload("res://ui/battle_preparation/unit_army_panel_container.tscn")
+const convoy_item_panel_container_scene = preload("res://ui/battle_preparation/convoy_item_panel_container.tscn")
+
+var playerOverworldData : PlayerOverworldData
+
+@onready var temp = 4
+
+func _ready():
+	if !playerOverworldData:
+		playerOverworldData = PlayerOverworldData.new()
+		
+
+func set_po_data(po_data):
+	playerOverworldData = po_data
+
+func fill_army_scroll_container():
+	for unit in playerOverworldData.total_party:
+		var unit_army_panel_container = unit_army_panel_container_scene.instantiate()
+		unit_army_panel_container.unit = unit
+		unit_army_panel_container.set_po_data(playerOverworldData)
+		main_scroll_container.add_child(unit_army_panel_container)
+		unit_army_panel_container.focus_entered.connect(unit_focus_entered.bind(unit))
+		unit_army_panel_container.unit_selected.connect(_on_unit_selected)
+		unit_army_panel_container.unit_deselected.connect(_on_unit_deselected)
+	var test = main_scroll_container.get_child(0)
+	if(test):
+		test.grab_focus()
+
+func clear_scroll_scontainer():
+	var children = main_scroll_container.get_children()
+	for child in children:
+		child.queue_free()
+
+func fill_convoy_scroll_container():
+	var i = 0
+	while i < temp:
+		var convoy_item_panel_container = convoy_item_panel_container_scene.instantiate()
+		main_scroll_container.add_child(convoy_item_panel_container)
+		convoy_item_panel_container.focus_entered.connect(item_focus_entered.bind(convoy_item_panel_container.item))
+		if i == 0:
+			convoy_item_panel_container.grab_focus()
+		i += 1
+
+func unit_focus_entered(unit):
+	unit_focused.emit(unit)
+
+func item_focus_entered(item):
+	item_focused.emit(item)
+
+func _on_unit_selected(unit):
+	#var current_selected_count = playerOverworldData.selected_party.size()
+	#var max_selected_count = playerOverworldData.available_party_capacity
+	#army_convoy_header.set_units_left_value(current_selected_count,max_selected_count)
+	unit_selected.emit(unit)
+
+func _on_unit_deselected(unit):
+	#var current_selected_count = playerOverworldData.selected_party.size()
+	#var max_selected_count = playerOverworldData.available_party_capacity
+	#army_convoy_header.set_units_left_value(current_selected_count,max_selected_count)
+	unit_deselected.emit(unit)
+
+func get_first_scroll_panel():
+	return main_scroll_container.get_child(0)
