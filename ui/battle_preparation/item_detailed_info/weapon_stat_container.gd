868fc2f79fc6c31b278589ea326614b6f4127ea6
Pellincr <craig.pelling@gmail.com>
2025-08-07 23:11:20 -0400

    Weapon Detailed View Updates

--- ui/battle_preparation/item_detailed_info/weapon_stat_container.gd
+++ ui/battle_preparation/item_detailed_info/weapon_stat_container.gd
@@ -5,7 +5,17 @@ extends HBoxContainer
 @onready var hit_value_label = $VBoxContainer/HitContainer/HitValueLabel
 @onready var critical_value_label = $VBoxContainer/CriticalContainer/CriticalValueLabel
 @onready var range_value_label = $VBoxContainer/RangeContainer/RangeValueLabel
+
+@onready var damage_type_icon = $VBoxContainer/DamageContainer/DamageTypeIcon
+
 @onready var requirements_icon_container = $VBoxContainer/RequirementsContainer/RequirementsIconContainer
+
+
+@onready var armored_icon = $VBoxContainer2/EffectiveContainer/EffectiveTraitIconContainer/ArmoredIcon
+@onready var mounted_icon = $VBoxContainer2/EffectiveContainer/EffectiveTraitIconContainer/MountedIcon
+@onready var flier_icon = $VBoxContainer2/EffectiveContainer/EffectiveTraitIconContainer/FlierIcon
+@onready var undead_icon = $VBoxContainer2/EffectiveContainer/EffectiveTraitIconContainer/UndeadIcon
+
 
 @onready var durability_value_label = $VBoxContainer2/DurabilityContainer/DurablityValueLabel
 @onready var weight_value_label = $VBoxContainer2/WeightContainer/WeightValueLabel
@@ -20,7 +30,26 @@ func set_damage_value(dmg):
 	damage_value_label.text = str(dmg)
 
 func set_scaling_value(scaler):
-	scaling_value_label.text = scaler #Unsure how this will be updated when weapons are updated
+	match scaler:
+		ItemConstants.SCALING_TYPE.STRENGTH:
+			scaling_value_label.text = "Strength"
+		ItemConstants.SCALING_TYPE.MAGIC:
+			scaling_value_label.text = "Magic"
+		ItemConstants.SCALING_TYPE.SKILL:
+			scaling_value_label.text = "Skill"
+		ItemConstants.SCALING_TYPE.CONSTITUTION:
+			scaling_value_label.text = "Constitution"
+		ItemConstants.SCALING_TYPE.NONE:
+			scaling_value_label.text = "-"
+
+func set_damage_type_icon(damage_type):
+	var physical_icon = preload("res://resources/sprites/icons/damage_icons/physical_damage_icon.png")
+	var magic_icon = preload("res://resources/sprites/icons/damage_icons/magic_damage_icon.png")
+	match damage_type:
+		ItemConstants.DAMAGE_TYPE.PHYSICAL:
+			damage_type_icon.texture = physical_icon
+		ItemConstants.DAMAGE_TYPE.MAGIC:
+			damage_type_icon.texture = magic_icon
 
 func set_hit_value(hit):
 	hit_value_label.text = str(hit)
@@ -42,16 +71,25 @@ func set_range_value(range):
 	else:
 		range_value_label.text = str(min)
 
-func fill_effective_trait_icons():
-	pass
+func set_effective_trait_visibility(effective_traits):
+	if effective_traits.has(unitConstants.TRAITS.ARMORED):
+		armored_icon.visible = true
+	if effective_traits.has(unitConstants.TRAITS.MOUNTED):
+		mounted_icon.visible = true
+	if effective_traits.has(unitConstants.TRAITS.FLIER):
+		flier_icon.visible = true
+	if effective_traits.has(unitConstants.TRAITS.UNDEAD):
+		undead_icon.visible = true
+
 
 func update_by_item():
 	if item is WeaponDefinition:
 		set_damage_value(item.damage)
-		#set_scaling_value(item.item_scaling_type)
+		set_damage_type_icon(item.item_damage_type)
+		set_scaling_value(item.item_scaling_type)
 		set_hit_value(item.hit)
 		set_critical_label(item.critical_chance)
 		set_weight_value(item.weight)
 		set_durability_value(item.uses)
 		set_range_value(item.attack_range)
-		#fill_effective_trait_icons()
+		set_effective_trait_visibility(item.weapon_effectiveness)
