7ca2e72f0015ebaa90a998a593b088e15c637227
pellincr <craig.pelling@gmail.com>
2025-09-28 18:23:29 -0400

    Persuader Item Update and Began Menu Creation

--- ui/battle_prep_new/convoy/convoy_panel/convoy_panel.gd
+++ ui/battle_prep_new/convoy/convoy_panel/convoy_panel.gd
@@ -2,8 +2,19 @@ extends PanelContainer
 
 signal convoy_panel_pressed()
 
+@onready var label: Label = $MarginContainer/Label
 
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		convoy_panel_pressed.emit()
+
+
+func _on_focus_entered() -> void:
+	#label.self_modulate = "FFFFFF"
+	theme = preload("res://ui/battle_prep_new/unit_army_panel/unit_panel_not_selected_hovered.tres")
+
+
+func _on_focus_exited() -> void:
+	#label.self_modulate = "828282"
+	theme = preload("res://ui/battle_prep_new/unit_army_panel/unit_panel_not_selected.tres")
