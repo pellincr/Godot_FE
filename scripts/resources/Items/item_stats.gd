1f6ff5d808fbaf3dcaaeb7e75184a8dad75c5b8b
Devin Murphy <demurphy1324@gmail.com>
2025-10-09 10:52:52 -0400

    event updates

--- scripts/resources/Items/item_stats.gd
+++ scripts/resources/Items/item_stats.gd
@@ -4,7 +4,7 @@ class_name ItemStats
 #@export_range(-1, 100, 1, "or_greater") var uses = 0
 @export_group("Item Stat Changes")
 @export var max_uses = 0
-@export_range(0,.01,1) var percent_durability: float = 1
+@export_range(0,1) var percent_durability: float = 1
 @export var ubreakable : bool = false
 @export_group("Bonus Stats From Held in Inventory")
 @export var inventory_bonus_stats : CombatUnitStat = null
