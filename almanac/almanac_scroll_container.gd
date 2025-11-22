c934584d3a4d49a304deb1c2f256f9843c08a003
pellincr <craig.pelling@gmail.com>
2025-11-22 13:48:53 -0500

    Item Info In the Almanac
    Almanac has Item Information, though it does not appropriately show if it is unlocked.

--- almanac/almanac_scroll_container.gd
+++ almanac/almanac_scroll_container.gd
@@ -15,6 +15,13 @@ const unit_type_almanac_detailed_view_scene = preload("res://almanac/unit_type/u
 const archetype_panel_container_scene = preload("res://almanac/archetype/archetype_panel_container.tscn")
 const archetype_detailed_view_scene = preload("res://almanac/archetype/archetype_detailed_view.tscn")
 
+
+const item_panel_container_scene = preload("res://ui/battle_prep_new/item_panel/item_panel_container.tscn")
+
+const weapon_detailed_info_scene = preload("res://ui/battle_prep_new/item_detailed_info/weapon_detailed_info.tscn")
+const equpiment_detailed_info_scene = preload("res://ui/battle_prep_new/item_detailed_info/equipment_detailed_info.tscn")
+const consumable_item_detailed_info_scene = preload("res://ui/battle_prep_new/item_detailed_info/consumable_item_detailed_info.tscn")
+
 var focused = false
 
 func _ready():
@@ -82,6 +89,52 @@ func fill_main_scroll_container_archetypes():
 			archetype_panel_container.grab_focus.call_deferred()
 			focused = true
 
+func fill_main_scroll_container_weapons():
+	var item_keys := ItemDatabase.items.keys()
+	var commander_weapon_keys := ItemDatabase.commander_weapons.keys()
+	for commander_weapon_key in commander_weapon_keys:
+		var item = ItemDatabase.commander_weapons[commander_weapon_key]
+		var item_panel_container = item_panel_container_scene.instantiate()
+		item_panel_container.item = item
+		main_scroll_container.add_child(item_panel_container)
+		item_panel_container.focus_entered.connect(_on_item_panel_focused.bind(item))
+		if !focused:
+			item_panel_container.grab_focus.call_deferred()
+			focused = true
+	for item_key in item_keys:
+		var item = ItemDatabase.items[item_key]
+		if item is WeaponDefinition:
+			var item_panel_container = item_panel_container_scene.instantiate()
+			item_panel_container.item = item
+			main_scroll_container.add_child(item_panel_container)
+			item_panel_container.focus_entered.connect(_on_item_panel_focused.bind(item))
+
+func fill_main_scroll_container_equpiment():
+	var item_keys := ItemDatabase.items.keys()
+	for item_key in item_keys:
+		var item = ItemDatabase.items[item_key]
+		if item.item_type == ItemConstants.ITEM_TYPE.EQUIPMENT:
+			var item_panel_container = item_panel_container_scene.instantiate()
+			item_panel_container.item = item
+			main_scroll_container.add_child(item_panel_container)
+			item_panel_container.focus_entered.connect(_on_item_panel_focused.bind(item))
+			if !focused:
+				item_panel_container.grab_focus.call_deferred()
+				focused = true
+
+func fill_main_scroll_container_consumable():
+	var item_keys := ItemDatabase.items.keys()
+	for item_key in item_keys:
+		var item = ItemDatabase.items[item_key]
+		if  item is ConsumableItemDefinition:
+			var item_panel_container = item_panel_container_scene.instantiate()
+			item_panel_container.item = item
+			main_scroll_container.add_child(item_panel_container)
+			item_panel_container.focus_entered.connect(_on_item_panel_focused.bind(item))
+			if !focused:
+				item_panel_container.grab_focus.call_deferred()
+				focused = true
+
 
 func clear_sub_children():
 	var children = detailed_view_sub_container.get_children()
@@ -100,8 +153,12 @@ func _on_almanac_header_swapped(almanac_state : AlmanacScrollHeader.ALMANAC_STAT
 			fill_main_scroll_container_commander_type()
 		AlmanacScrollHeader.ALMANAC_STATES.ARCHETYPES:
 			fill_main_scroll_container_archetypes()
-		AlmanacScrollHeader.ALMANAC_STATES.ITEMS:
-			pass
+		AlmanacScrollHeader.ALMANAC_STATES.WEAPONS:
+			fill_main_scroll_container_weapons()
+		AlmanacScrollHeader.ALMANAC_STATES.EQUIPMENT:
+			fill_main_scroll_container_equpiment()
+		AlmanacScrollHeader.ALMANAC_STATES.CONSUMABLES:
+			fill_main_scroll_container_consumable()
 
 func _on_unit_type_panel_focued(unit_type, unlocked):
 	clear_sub_children()
@@ -116,3 +173,20 @@ func _on_archetype_panel_focused(archetype:ArmyArchetypeDefinition,unlocked):
 	if unlocked:
 		archetype_detailed_view.archetype = archetype
 	detailed_view_sub_container.add_child(archetype_detailed_view)
+
+func _on_item_panel_focused(item:ItemDefinition):
+	clear_sub_children()
+	if item is WeaponDefinition:
+		var weapon_detailed_info = weapon_detailed_info_scene.instantiate()
+		weapon_detailed_info.item = item
+		detailed_view_sub_container.add_child(weapon_detailed_info)
+		weapon_detailed_info.update_by_item()
+	elif item is ConsumableItemDefinition:
+		var consumable_item_detailed_info = consumable_item_detailed_info_scene.instantiate()
+		consumable_item_detailed_info.item = item
+		detailed_view_sub_container.add_child(consumable_item_detailed_info)
+	elif item.item_type == ItemConstants.ITEM_TYPE.EQUIPMENT:
+		var equpiment_detailed_info = equpiment_detailed_info_scene.instantiate()
+		equpiment_detailed_info.item = item
+		detailed_view_sub_container.add_child(equpiment_detailed_info)
+		
